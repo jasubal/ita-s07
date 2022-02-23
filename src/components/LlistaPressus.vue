10b6918474c7d6d2205f96fd620db9b80bad50c2
Jaume Subirats <jaume.subirats@gmail.com>
2022-02-23 02:40:39 +0100

    Ex7 beginning

--- src/components/LlistaPressus.vue
+++ src/components/LlistaPressus.vue
@@ -2,8 +2,16 @@
 <div id="c-llistaPressus" class="c-modul">
 
 <div v-if="pressusList.length > 0 " id="llistat">
-<h2>Listat de Pressupostos</h2>
-<p>Pressupostos guardats: {{pressusList.length}}</p>
+<h2>Listat de Pressupostos <span>({{pressusList.length}})</span></h2>
+<div class="c-group">
+<input type="search" v-model.lazy="search" placeholder="Cerca" />
+</div>
+<div class="c-group">
+    <button @click="sortBy='nomPressu'">Ordena A-Z</button>
+    <button @click="sortBy='preuTotal'">Ordena Preu</button>
+    <button @click="sortList()">test</button>
+</div>
+ <pre>{{pressusList2sort}}</pre>
 <ul>
     <li class="pressItem" v-for="(item,index ) in pressusList" :key='index' >
         <div v-html="renderPressu(item)"></div>
@@ -13,54 +21,91 @@
 <!--
       <pre>{{ pressusList }}</pre>
  -->
+
 </div>
 
 </div>
 </template>
 
-
-
 <script>
 export default {
-
 
     name: "LlistaPressus",
     props: ["pressusList"],
     data() {
         return {
-
+            sortBy: null,
+            pressusList2sort: [],
         }
     },
+    watch: {
+        sortBy: function(){
+            this.sortList() },
+    },
     methods: {
+sortList(){
+this.pressusList2sort = this.pressusList.slice();
+if (this.sortBy === 'nomPressu') {
+    console.log('sort by nomPressu');
+this.pressusList2sort.sort(function(a,b) {
+        if (a.pressupost[0].nomPressu < b.pressupost[0].nomPressu) { return -1; }
+        if (a.pressupost[0].nomPressu > b.pressupost[0].nomPressu) { return 1; }
+        return 0;
+    });
+} else if (this.sortBy === 'preuTotal') {
+    console.log('sort by preuTotal');
+this.pressusList2sort .sort(function(a,b) {
+        if (a.pressupost[0].preuTotal < b.pressupost[0].preuTotal) { return -1; }
+        if (a.pressupost[0].preuTotal > b.pressupost[0].preuTotal) { return 1; }
+        return 0;
+    });
+} else {
+    console.log('sort by null');
+    // reset sort
+ this.pressusList2sort.sort(function(a,b) { if (a.pressupost[0].id < b.pressupost[0].id) { return -1; }
+        if (a.pressupost[0].id > b.pressupost[0].id) { return 1; }
+        return 0;
+    });
+}
+},
 
     renderPressu(item) {
     let output = '';
     let resum = item.pressupost[0]
-/* */
-output += "<strong>Nom presupost: "+resum.nomPressu+ "</strong><br>";
-output += "Client: "+resum.nomClient+"<br>";
-output += "Data: "+resum.dataPressu+" / ";
-output += "ID: "+resum.id+"<br>";
-output += "<strong>Inclou: </strong> ";
-for(let i=1; i<item.pressupost.length; i++) {
-    if (item.pressupost[i].langs > 0) {
-    let totalServeisWeb = item.pressupost[i].langs * item.pressupost[i].pags * 30;
+    output += "<strong>Nom presupost: "+resum.nomPressu+ "</strong><br>";
+    output += "Client: "+resum.nomClient+"<br>";
+    output += "Data: "+resum.dataPressu+" / ";
+    output += "ID: "+resum.id+"<br>";
+    output += "<strong>Inclou: </strong> ";
+        for(let i=1; i<item.pressupost.length; i++) {
+        if (item.pressupost[i].langs > 0) {
+        let totalServeisWeb = item.pressupost[i].langs * item.pressupost[i].pags * 30;
     output += item.pressupost[i].servei + ": "+item.pressupost[i].preu+"€<br>";
     output += item.pressupost[i].langs + " idioma/es + "+item.pressupost[i].pags+" pàgina/es : "+totalServeisWeb+"€<br>";
-    } else {
+        } else {
     output += item.pressupost[i].servei + ": "+item.pressupost[i].preu+"€<br>";
+        }
     }
-}
-output += "<strong>Preu total: "+resum.preuTotal+"€</strong><br>";
+    output += "<strong>Preu total: "+resum.preuTotal+"€</strong><br>";
     return output;
 }
-    },
 
+},
+// computed: {
 
+// }
 }
 </script>
 
 <style>
+h2 span { font-size: 0.6em; }
+.c-group {
+    display: flex;
+    justify-content: center;
+    margin: 12px;
+}
+.c-group  input { border: 1px solid #e0e0e0; max-width: 80%; }
+.c-group  button { font-size: .8em; margin: 2px; }
 #llistat ul {
     flex-wrap: wrap;
     align-items: center;
--- src/components/MostraPressu.vue
+++ src/components/MostraPressu.vue
@@ -1,87 +1,64 @@
 <template>
-<div id="c-mostraPressu" class="c-modul">
-
-<div v-if="preuTotal > 1 " id="elPressu">
-<h2>Pressupost Actual</h2>
-<div id="dadesPressu" v-html="renderPressu()"></div>
-
-</div>
-
-
-
-</div>
-
+  <div id="c-mostraPressu" class="c-modul">
+    <div v-if="preuTotal > 1" id="elPressu">
+      <h2>Pressupost Actual</h2>
+      <div id="dadesPressu" v-html="renderPressu()"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-    name: "MostraPressu",
-    props: ["currentPressu", "preuTotal"],
-    data() {
-        return {
-        }
-    },
-    methods: {
+  name: "MostraPressu",
+  props: ["currentPressu", "preuTotal"],
+  data() {
+    return {};
+  },
+  methods: {
     renderPressu() {
-    let output = '';
-    let resum = this.currentPressu[0];
-    //let productes = this.currentPressu[1];
-    //let dataActual = new Date().toLocaleDateString()
+      let output = "";
+      let resum = this.currentPressu[0];
 
-    output += '<h3 class="tit">Resum</h3>';
-    resum.nomPressu !== '' ? output += 'Nom pressupost: '+ resum.nomPressu + '</br>' : 0;
-    resum.nomClient !== '' ? (output += 'Client: ' + resum.nomClient + '</br>' ) : 0;
-    output += 'Data pressupost: '+ resum.dataPressu + '</br>';
+      output += '<h3 class="tit">Resum</h3>';
+      resum.nomPressu !== ""
+        ? (output += "Nom pressupost: " + resum.nomPressu + "</br>")
+        : 0;
+      resum.nomClient !== ""
+        ? (output += "Client: " + resum.nomClient + "</br>")
+        : 0;
+      output += "Data pressupost: " + resum.dataPressu + "</br>";
 
-    output += '<h3 class="tit">Productes</h3>';
+      output += '<h3 class="tit">Productes</h3>';
 
-this.currentPressu.forEach((item,index) => {
-    if (index > 0) {
-        if (item.langs > 0) {
-        let totalServeisWeb = item.langs * item.pags * 30;
-        output += item.servei + ': ' + item.preu + '€<br>';
-        output += item.langs + ' idioma/es + '+ item.pags + ' pàgina/es : '+ totalServeisWeb + '€<br>';
-        } else {
-        output += item.servei + ': ' + item.preu + '€<br>';
+      this.currentPressu.forEach((item, index) => {
+        if (index > 0) {
+          if (item.langs > 0) {
+            let totalServeisWeb = item.langs * item.pags * 30;
+            output += item.servei + ": " + item.preu + "€<br>";
+            output += item.langs + " idioma/es + " + item.pags + " pàgina/es : " + totalServeisWeb + "€<br>";
+          } else { output += item.servei + ": " + item.preu + "€<br>"; }
         }
-    }
-});
+      });
 
-    output += '<br>';
-    output += '<strong>Preu Total: ' + resum.preuTotal + '€</strong></br>';
+      output += "<br>";
+      output += "<strong>Preu Total: " + resum.preuTotal + "€</strong></br>";
 
-/*
-
-});
-
-    productes.forEach((producte) => {
-    if (producte.langs > 0 && producte.pags > 0) {
-        output += producte.nom + ': '+ producte.preu + '€</br>';
-        output += 'Idioma/es ' + producte.langs + ' - Pàgina/es '+  producte.pags ;
-    let totalServeisWeb = producte.langs * producte.pags * 30;
-    output += ': ' + totalServeisWeb + '€<br>';
-
-    } else {
-        output += producte.nom + ': '+ producte.preu + '€</br>';
-        }
-    });
-    output += '<br>';
-    output += '<strong>Preu Total: ' + resum.preuTotal + '€</strong></br>';
-*/
-
-    return output
-    }
-
-    }
-
-
-
-}
+      return output;
+    },
+  },
+};
 </script>
 
 <style scoped>
-#c-mostraPressu { padding: 0; }
-#elPressu { height: 100%; margin: 0; }
-#dadesPressu { text-align: left; padding: 0 2em; }
+#c-mostraPressu {
+  padding: 0;
+}
+#elPressu {
+  height: 100%;
+  margin: 0;
+}
+#dadesPressu {
+  text-align: left;
+  padding: 0 2em;
+}
 </style>
